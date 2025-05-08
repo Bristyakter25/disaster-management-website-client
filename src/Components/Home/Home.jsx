e05b11d80ea4d71784d770b559aeb7c428f2a8a3
Bristyakter25 <sabihaakterbristy@gmail.com>
2025-05-09 03:22:49 +0600

    added latest alert panel section

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner';
+import LiveAlerts from './LiveAlerts';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <LiveAlerts></LiveAlerts>
         </div>
     );
 };
--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import Home from "../Components/Home/Home";
 import ErrorPage from "../pages/ErrorPage";
+import AddAlertPanels from "../pages/AddAlertPanels";
 
 export const router = createBrowserRouter([
     {
@@ -14,6 +15,10 @@ export const router = createBrowserRouter([
         path: "/",
         element: <Home></Home>,
       },
+      {
+        path:"addAlertPanels",
+        element:<AddAlertPanels></AddAlertPanels>
+      }
      
     ]
     },
