91285846cba7ee8e83a20f320294c0f3ba230974
TinniEra <nafisaakterera@gmail.com>
2025-06-13 22:07:32 +0600

    testing the branch

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -2,16 +2,19 @@ import { createBrowserRouter } from "react-router-dom";
 
 import MainLayout from "../Layout/MainLayout";
 import Home from "../Components/Home/Home";
+import ErrorPage from "../pages/ErrorPage";
 
 export const router = createBrowserRouter([
     {
       path: "/",
       element: <MainLayout></MainLayout>,
+      errorElement: <ErrorPage></ErrorPage>,
       children: [
         {
         path: "/",
         element: <Home></Home>,
-      }
+      },
+     
     ]
     },
   ]);
